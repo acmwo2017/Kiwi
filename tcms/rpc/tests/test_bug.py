b0afaf8932592e85c3cba95a5e75bee3ccfdee9a
Anton Sankov <a.sankov96@gmail.com>
2020-09-03 15:23:48 +0300

    Add tests for `Bug.details` API method

---
+++ tcms/rpc/tests/test_bug.py
@@ -0,0 +1,22 @@
+from django.core.cache import cache
+
+from tcms.rpc.tests.utils import APITestCase
+
+class TestBug(APITestCase):
+
+    def test_get_details_from_cache(self):
+        url = "http://some.url"
+        expected_result = {
+            'title': 'Bug from cache',
+            'description': 'This bug came from the Django cache'
+        }
+        cache.set(url, expected_result)
+        result = self.rpc_client.Bug.details(url)
+
+        self.assertEqual(result, expected_result)
+
+    def test_empty_details_when_tracker_does_not_exist(self):
+        url = "http://unknown-tracker.url"
+
+        result = self.rpc_client.Bug.details(url)
+        self.assertEqual(result, {})
